4066c55a521c7d70ed8e6ca5739be768ca456721
UnidentifiedSecTM <59448060+Himosis@users.noreply.github.com>
2020-02-27 13:33:59 +0330

    Create UDF1.sh

---
+++ UDF1.sh
@@ -0,0 +1,317 @@
+#!/system/xbin/bash
+#coded by J4riko
+
+############################################
+# CTRL + C                                 #
+############################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -b $green"[#]> (ctrl + c) Detected, trying to Exit...." | lolcat 
+echo -b $red"[#]> MahdiiHC " | lolcat
+echo -b $blue"[#]> SEE YOU AGAIN:)..." | lolcat
+sleep 1
+exit 
+}
+
+clear
+toilet -f mono12 -F gay "UDF"
+sleep 1
+echo "----------------------------" | lolcat
+sleep 1
+echo "<|[Unidentified security team]|>" | lolcat
+sleep 1
+echo "----------------------------" | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo "in the name of Allah        " | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo "Coded By : J4riko            " | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo "Team => Unidentified" | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo "CHannel Tel => T.ME/UniDentiFied_TM" | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo "We are the future of Iran" | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo "Thanks to all members UniDentiFied" | lolcat
+sleep 1
+echo "__________            " | lolcat
+sleep 1
+echo " Contact us ==> http://T.ME/H43ER" | lolcat
+sleep 1
+echo "__________________________" | lolcat
+echo "1. LordDdos(Ddos Attack) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "2. Hacked16-Ddos(Ddos Attack) "| lolcat
+sleep 1
+echo "____________" | lolcat
+echo "3. Tool-X(install hacking tools) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "4. BlackEye(fishing tools) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "5. Weeman(fishing tools) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "6. D-TECH(information) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "7. lazymux(install hacking tools) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "8. OWscan(scan site) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "9. IPGeoLocation (Location) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "10. Crips (ip tool) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "11.Tm-Scanner (scan tool) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "12.commix(SecScanner-Bug finder-kali tool) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1 
+echo "13.DAT(dost attack tool) "| lolcat
+sleep 1 
+echo "____________" | lolcat
+sleep 1
+echo "14.A-Rat(Exploitation tool) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "15.GoldenEye(Dos Tool) "| lolcat
+sleep 1
+echo "____________" | lolcat
+sleep 1
+echo "16. Update UDF "| lolcat
+
+
+
+echo "00. Exit                  "| lolcat
+read J4riko
+
+if
+[ $J4riko = 1 ] || [ $J4riko = 1 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Mahdiihack/LordDdos.git
+clear
+ cd LordDdos
+python2 LordDdos.py
+fi
+
+if
+[ $J4riko = 2 ] || [ $J4riko = 2 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Mahdiihack/Hacked16-Ddos.git
+clear
+ cd Hacked16-Ddos
+python2 Hacked16-Ddos.py
+fi
+
+if
+[ $J4riko = 3 ] || [ $J4riko = 3 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/rajkumardusad/Tool-X.git
+clear
+ cd Tool-X
+sh install.aex
+fi
+
+if
+[ $J4riko = 4] || [ $J4riko = 4 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/thelinuxchoice/blackeye.git
+clear
+ cd blackeye
+bash blackeye.sh
+fi
+
+if
+[ $J4riko = 5] || [ $J4riko = 5 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/evait-security/weeman.git
+clear
+ cd weeman
+ python2 weeman.py
+ fi
+ 
+ if
+[ $J4riko = 6 ] || [ $J4riko = 6 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/shawarkhanethicalhacker/D-TECT.git
+clear
+ cd D-TECH
+pip2 install requests
+python2 d-tech.py
+fi
+
+if
+[ $J4riko = 7 ] || [ $J4riko = 7 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Gameye98/Lazymux.git
+clear
+ cd Lazymux
+python2 lazymux.py
+fi
+
+if
+[ $J4riko = 8 ] || [ $J4riko = 8 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Gameye98/OWScan.git
+clear
+ cd OWScan
+php owscan.php
+fi
+
+if
+[ $J4riko = 9 ] || [ $J4riko = 9 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/maldevel/IPGeoLocation.git
+clear
+ cd IPGeoLocation
+pip install -r requirements.txt
+python ipgeolocation.py -t
+fi
+
+if
+[ $J4riko = 10 ] || [ $J4riko = 10 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Manisso/Crips.git
+clear
+ cd Crips
+./install.sh
+python2 crips.py
+fi
+
+if
+[ $J4riko = 11 ] || [ $J4riko = 11 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/TechnicalMujeeb/TM-scanner.git
+clear
+ cd TM-scanner
+ sh install.sh
+ python2 tmscanner.py
+ fi
+ 
+if
+[ $J4riko = 12 ] || [ $J4riko = 12 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/commixproject/commix.git
+clear
+ cd commix
+python2 commix.py
+fi
+
+if
+[ $J4riko = 13 ] || [ $J4riko = 13 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/verluchie/dost-attack.git
+clear
+ cd dost-attack
+chmod 777 install.sh
+./install.sh
+./dost
+fi
+
+if
+[ $J4riko = 14 ] || [ $J4riko = 14 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Xi4u7/A-Rat.git
+clear
+ cd A-Rat
+python2 A-Rat.py
+fi
+
+if
+[ $J4riko = 15 ] || [ $J4riko = 15 ]
+then
+clear
+pkg install git -y
+clear
+git clone git clone https://github.com/jseidl/GoldenEye.git
+clear
+ cd GoldenEye
+ python2 goldeneye.py
+fi
+
+if
+[ $J4riko = 16 ] || [ $J4riko = 16 ]
+then
+clear
+pkg install git -y
+clear
+git clone https://github.com/Himosis/UDF-Tools.git
+clear
+ cd UDF-Tools
+bash UDF.sh
+fi
